f268dd35d9cc12dd15d4509e2df34c82b27c7e54
Надя <nadya.ptz@gmail.com>
2025-01-13 23:00:26 +0300

    2024/03/01 00:00|Домашнее задание по теме "Использование БД в маршрутизации. 1.1"

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-from routers import task, user
+from app.routers import task, user
 
 
-# python -m uvicorn main:app
+# python -m uvicorn app.main:app --reload
 
 app = FastAPI()
+
 
 @app.get("/")
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
+
 
 app.include_router(task.router)
 app.include_router(user.router)
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,23 +1,86 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+# Сессия БД
+from sqlalchemy.orm import Session
+# Функция подключения к БД
+from app.backend.db_depends import get_db
+# Аннотации, Модели БД и Pydantic.
+from typing import Annotated
+from app.models import User
+from app.schemas import CreateUser, UpdateUser
+# Функции работы с записями.
+from sqlalchemy import insert, select, update, delete
+# Функция создания slug-строки
+from slugify import slugify
 
 router = APIRouter(prefix="/user", tags=['user'])
 
+
 @router.get('/')
-async def all_users():
-    pass
+async def all_users(db: Annotated[Session, Depends(get_db)]):
+    users = db.scalars(select(User)).all()
+    return users
+
 
 @router.get('/user_id')
-async def user_by_id():
-    pass
+async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='No such user'
+        )
+    return user
+
 
 @router.post('/create')
-async def create_user():
-    pass
+async def create_user(db: Annotated[Session, Depends(get_db)], create_user: CreateUser):
+    # Проверка существования пользователя с таким username
+    existing_user = db.execute(select(User).
+                               where(User.username == create_user.username)).fetchone()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this username already exists"
+        )
+    db.execute(insert(User).values(username=create_user.username,
+                                   slug=slugify(create_user.username),
+                                   firstname=create_user.firstname,
+                                   lastname=create_user.lastname,
+                                   age=create_user.age
+                                   ))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+
 
 @router.put('/update')
-async def update_user():
-    pass
+async def update_user(db: Annotated[Session, Depends(get_db)], update_user: UpdateUser, user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='No such user'
+        )
+    db.execute(update(User).where(User.id==user_id).values(firstname=update_user.firstname,
+                                                           lastname=update_user.lastname,
+                                                           age=update_user.age
+
+    ))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+
+
 
 @router.delete("/delete")
-async def delete_user():
-    pass
+async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='No such user'
+        )
+    db.execute(delete(User).where(User.id==user_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User delete is successful!'}
+
+
